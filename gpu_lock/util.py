ee907f5c02a1ffcbc1a42a38fd2071f6d3787e1c
Misha Denil <misha.denil@gmail.com>
2014-10-23 09:51:35 +0100

    Properly detect running processes from other users.
    gpu_lock_info would incorrectly report locking processes as dead if you
    didn't have permission to send signals to them.  This fix makes it
    correctly distinguish between dead processes and those owned by other
    users.
    
    Fixes #5

--- gpu_lock/util.py
+++ gpu_lock/util.py
@@ -1,4 +1,5 @@
 import os
+import errno
 
 def process_exists(pid):
     if not pid:
@@ -7,8 +8,11 @@ def process_exists(pid):
     # http://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid
     try:
         os.kill(pid, 0)
-    except OSError:
-        return False
-    else:
-        return True
+    except OSError as e:
+        # ESRCH means no process found, we get EPERM if the process exists but
+        # we don't have permission to signal it.
+        if e.errno == errno.ESRCH:
+            return False
+
+    return True
 
